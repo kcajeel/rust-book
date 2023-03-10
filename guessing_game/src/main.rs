2c7b8df4a52439c14ba0e9176981090dd4278966
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2023-03-10 12:46:49 -0500

    added first 2 projects

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io as deez;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+        let _apples = 5;
+
+        deez::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a number.");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low"),
+            Ordering::Equal => {
+                println!("You won!");
+                break;
+            }
+            Ordering::Greater => println!("Too high"),
+        }
+    }
+}
