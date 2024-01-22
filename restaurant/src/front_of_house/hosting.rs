610afcec2c3f15a04b6f67e3cf77e59af3e34075
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-01-22 11:29:24 -0500

    added modules to restaurant

--- restaurant/src/front_of_house/hosting.rs
+++ restaurant/src/front_of_house/hosting.rs
@@ -1,3 +1,23 @@
+pub struct Menu {
+pub appetizers: Vec<String>,
+pub entrees: Vec<String>,
+pub desserts: Vec<String>,
+}
+
+impl Menu {
+    pub fn new(this_appetizer: String, this_entree: String, this_dessert: String) -> Self{
+        Self {
+            appetizers: vec![this_appetizer],
+            entrees: vec![this_entree],
+            desserts: vec![this_dessert],
+        }
+    }
+
+    pub fn get_entrees(menu: &Menu) -> Vec<String> {
+        menu.entrees.clone()
+    }
+}
+
 pub fn add_to_waitlist() {}
 
 fn seat_at_table() {}
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -14,11 +14,14 @@ pub enum MenuItem {
     Desert(Item),
 }
 
-
 pub fn eat_at_restaurant() {
     //absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
     //relative path
     front_of_house::hosting::add_to_waitlist();
+
+    let menu = front_of_house::hosting::Menu::new("calamari".to_owned(), "pizza".to_owned(), "cake".to_owned());
+    let entrees = front_of_house::hosting::Menu::get_entrees(&menu);
+    let appetizers = menu.appetizers;
 }
