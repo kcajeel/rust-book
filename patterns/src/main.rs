dd5f847da09f98205d14f2351a77d83785109707
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-05-08 18:41:43 -0400

    added patterns and matching

---
+++ patterns/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!(
+        "I read the chapter on Patterns and Matching, \nbut there weren't any example scenarios like in other chapters. \nThis chapter was more like a reference sheet than a tutorial since patterns have already appeared and been used in the Book."
+    );
+}
