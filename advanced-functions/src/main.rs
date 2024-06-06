982aefacebd1f9f1fdb32af5fab40b451dd6661d
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-06-06 13:20:51 -0400

    added advanced functions

---
+++ advanced-functions/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let answer = do_twice(add_one, 23);
+    println!("The answer is: {}", answer);
+}
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
