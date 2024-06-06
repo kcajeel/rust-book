fdb7f0186d954465cc099e2a6811e103f396422b
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-06-06 12:17:01 -0400

    added advanced traits

---
+++ advanced-traits/src/main.rs
@@ -0,0 +1,79 @@
+use core::str;
+use std::ops::Add;
+
+fn main() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+    println!("Default type parameter addition worked!");
+
+    assert_eq!(
+        Millimeters(1001),
+        Millimeters(1) + Meters(1)
+    );
+    println!("Customized type parameter addition worked!");
+
+    let person = Human;
+    person.fly();
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+    <Human as Wizard>::fly(&person);
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Point {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+#[derive(PartialEq, Clone, Copy, Debug)]
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, rhs: Meters) -> Self::Output {
+        Millimeters(self.0 + (rhs.0 * 1000))
+    }
+}
+
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking..");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("flapping arms around");
+    }
+}
