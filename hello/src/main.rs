7253c6b86689bef37666e33c8db99bbf40617b0b
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-06-10 15:06:50 -0400

    spawn a thread for each request now (bad)

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -10,9 +10,10 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        println!("Connection established");
-        handle_connection(stream);
+        
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
